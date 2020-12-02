bdfbd80801b97aa8d3e25becf94fd587d0cf50a2
kamil2789 <kamil2789@wp.pl>
2020-12-02 06:56:59 +0100

    hello openGL

---
+++ MyOpenGL/src/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    std::cout<<"Hello OpenGL";
+    
+    return 0;
+}
